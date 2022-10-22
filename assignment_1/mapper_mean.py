27a764aa23153c3a420ead29fdfe4ab4c669dc0e
Maksimov-Dmitry <61335724+Maksimov-Dmitry@users.noreply.github.com>
2022-10-22 23:44:21 +0200

    fix

--- assignment_1/mapper_mean.py
+++ assignment_1/mapper_mean.py
@@ -4,8 +4,15 @@ import csv
 
 chunk_size = 0 
 chunk_sum = 0.0 
+bad_line = None
 for line in sys.stdin:
+    if bad_line:
+        line = ''.join([bad_line, line])
+        bad_line = None
     data = next(csv.reader([line], delimiter=','))
+    if len(data) < 10:
+        bad_line = line
+        continue
     try:
         chunk_sum += float(data[9])
         chunk_size += 1 
--- assignment_1/mapper_variance.py
+++ assignment_1/mapper_variance.py
@@ -4,9 +4,16 @@ import csv
 
 chunk_size = 0 
 chunk_sum = 0.0
-chunk_sum_2 = 0.0 
+chunk_sum_2 = 0.0
+bad_line = None
 for line in sys.stdin:
+    if bad_line:
+        line = ''.join([bad_line, line])
+        bad_line = None
     data = next(csv.reader([line], delimiter=','))
+    if len(data) < 3:
+        bad_line = line
+        continue
     try:
         chunk_sum += float(data[9])
         chunk_sum_2 += float(data[9]) ** 2
@@ -14,6 +21,6 @@ for line in sys.stdin:
     except:
         continue
 
-variance = (chunk_sum_2 / chunk_size - (chunk_sum / chunk_size) ** 2) * chunk_size / (chunk_size - 1)
+variance = chunk_sum_2 / chunk_size - (chunk_sum / chunk_size) ** 2
 mean = chunk_sum / chunk_size
 print(chunk_size, mean, variance)
